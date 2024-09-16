212b37eba96d7afde04be168ff445372f1cb9660
rujbin <115537871+rujbin@users.noreply.github.com>
2024-09-16 14:03:46 +0200

    Update cnn.py

--- udemyKurs/cnn.py
+++ udemyKurs/cnn.py
@@ -1,1 +1,16 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten
 
+model = Sequential()
+
+model.add(Conv2D(30, kernel_size=(3, 3), activation="relu", input_shape=(28, 28, 1)))
+model.add(Flatten())
+model.add(Dense(10, activation="softmax"))
+
+model.compile(optimizer="rmsprop", loss="categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(
+    X_train.reshape(60000, 28, 28, 1),
+    y_train,
+    epochs=80,
+    batch_size=1000)
