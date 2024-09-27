0e52b6aaf77b4b89c17f7cd2dd980a620f170016
rujbin <115537871+rujbin@users.noreply.github.com>
2024-09-27 14:00:15 +0200

    Add files via upload

---
+++ udemyKurs/Bilderkennung Auto/Neuronales Netz bauen(3).py
@@ -0,0 +1,37 @@
+from tensorflow.keras.datasets import cifar10
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
+
+X_train = X_train.astype(np.float32) / 255.
+X_test = X_test.astype(np.float32) / 255.
+
+print(y_train[4])
+plt.imshow(X_train[4])
+plt.show()
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), input_shape=(32, 32, 3), activation="relu"))
+model.add(Conv2D(32, kernel_size=(3, 3), activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, kernel_size=(3, 3), activation="relu"))
+model.add(Conv2D(64, kernel_size=(3, 3), activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Flatten())
+model.add(Dense(128, activation="relu"))
+model.add(Dense(1, activation="sigmoid"))
+
+model.compile(optimizer="rmsprop", loss="binary_crossentropy", metrics=["accuracy"])
+
+y_train_car = y_train == 1
+model.fit(X_train, y_train_car, batch_size=128, epochs=10, shuffle=True)
+
+print(model.evaluate(X_train, y_train_car))
+
+y_test_car = y_test == 1
+print(model.evaluate(X_test, y_test_car))
